ca05ed2066dd7c797d623f052da0fa01e266e774
Drazy <kexin19991018@126.com>
2021-05-08 18:36:02 +0800

    初始化建造者模式

---
+++ src/components/builder/BuilderIndex.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export interface IBuilderIndexProps {}
+
+export default class BuilderIndex extends React.Component<IBuilderIndexProps> {
+    public render() {
+        return (
+            <div>
+                <h2>建造者设计模式（待学习）</h2>
+            </div>
+        );
+    }
+}
